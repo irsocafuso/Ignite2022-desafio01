01bb0461d4fe8eeb2bcf4ffbfcb13b0c7d3bcb83
Wilson Sanches Filho <wilsinho_sanches@hotmail.com>
2023-02-11 11:51:55 -0300

    finished styles of task list

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,8 +1,46 @@
+import { useCallback } from 'react';
 import Clipboard from '../../assets/clipboard.svg'
+import Trash from '../../assets/trash.svg'
 
 import styles from './TaskList.module.css'
 
 const TaskList = () => {
+  const test = false;
+
+  const renderTaskList = useCallback(() => {
+    if (test) {
+      return (
+        <div className={styles.empty}>
+          <img src={Clipboard} alt="No Tasks" />
+          <p>Você ainda não tem tarefas cadastradas <br />
+            <span>Crie tarefas e organize seus itens a fazer</span></p>
+        </div>
+      )
+    }
+
+    return (
+      <li className={styles.list}>
+        <ul className={styles['task-list']}>
+          <div className={styles['task-checkbox']}>
+            <input type="checkbox" id='task-checkbox' />
+            <label htmlFor="task-checkbox"></label>
+          </div>
+
+          <p>Teste</p>
+          <img src={Trash} alt="No Tasks" />
+        </ul>
+        <ul className={styles['task-list']}>
+          <div className={styles['task-checkbox']}>
+            <input type="checkbox" id='task-checkbox' />
+            <label htmlFor="task-checkbox"></label>
+          </div>
+          <p>Teste</p>
+          <img src={Trash} alt="No Tasks" />
+        </ul>
+      </li>
+    )
+
+  }, [test])
 
   return (
     <div className={styles['task-list']}>
@@ -11,11 +49,7 @@ const TaskList = () => {
         <p className={styles.done}>Concluídas <span className={styles.counter}>0</span></p>
       </div>
       <div className={styles.body}>
-        <div className={styles.empty}>
-          <img src={Clipboard} alt="No Tasks" />
-          <p>Você ainda não tem tarefas cadastradas <br />
-            <span>Crie tarefas e organize seus itens a fazer</span></p>
-        </div>
+        {renderTaskList()}
       </div>
     </div>
   );
